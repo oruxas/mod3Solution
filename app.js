f93734f9c3aadef4470182bce8d2a3ba4a2b3a3e
Evaldas <evaldaskal@gmail.com>
2016-10-03 20:36:20 +0300

    Lets go

---
+++ app.js
@@ -0,0 +1,29 @@
+( function(){
+
+   var NarrowItDownAppp = angular.module('NarrowItDownApp',[]);
+
+    NarrowItDownApp.controller('NarrowItDownController', ['$scope', function($scope) {
+    $scope.greeting = 'Hola!';
+    }])
+    .service('MenuSearchService', function(){
+        var menuSearch = this;
+        menuSearch.getMatchedMenuItems = function(searchTerm) { //input searchTerms
+            return $http({
+                method: 'GET',
+                url: 'https://davids-restaurant.herokuapp.com/menu_items.json'
+            }).then(function (result) {
+            // process result and only keep items that match
+            var data;
+            // for(data in result.data){
+
+            // }
+                //sconsole.log(result.data);
+            var foundItems = [];
+
+            // return processed items
+            return foundItems;
+        });
+        }
+    })
+
+}());
