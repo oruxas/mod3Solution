042581b94c203264de213247da1a8e9d269be488
Evaldas <evaldaskal@gmail.com>
2016-10-08 22:57:27 +0300

    final solution

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 
     function FoundItemsDirective(){
         var ddo = {                         //directive definition object
-            template: '<div> <ul data-ng-model="narrowit.found"> <li ng-repeat="item in narrowit.items"> {{item.name}} ({{item.short_name}}) {{item.description}}  <button ng-click="narrowit.onRemove({index: $index})">Dont want this one</button></li> </ul></div>',
+            template: '<div> <ul > <li ng-repeat="item in narrowit.items"> {{item.name}} ({{item.short_name}}) {{item.description}}  <button ng-click="narrowit.onRemove({index: $index})">Dont want this one</button></li> </ul></div>',
             restrict: 'E',
             scope: {
                 items: "<",
@@ -27,16 +27,25 @@
     function NarrowItDownController($scope, MenuSearchService) {
        var narrowit = this;    
        var found=[];
-       narrowit.getlist = function(searctTerm){
-
-             MenuSearchService.getMatchedMenuItems(narrowit.searchTerm).then(function(result){
-                // console.log(Object.getOwnPropertyNames(result));
-
-                narrowit.found = result;
-                console.log(narrowit.found);
-
+       narrowit.searchTerm="";
+       //narrowit.showError = true;
+       narrowit.getlist = function(searchTerm){
+           console.log('click');
+           if( (narrowit.searchTerm.length === 0 || (narrowit.searchTerm == ""))){
+                narrowit.showError = true;
+                console.log(narrowit.showError);
+               narrowit.found = [];
+              
+           } else{
+               
+               MenuSearchService.getMatchedMenuItems(narrowit.searchTerm).then(function(result){
+                // console.log(Object.getOwnPropertyNames(result)); 
+                     narrowit.found = result;
+                    console.log(narrowit.found);
+                    narrowit.showError = false;
              }); 
-                        
+           }
+           
         }; //end of getList
         narrowit.removeItem = function (itemIndex) {
                     //console.log(itemIndex);
@@ -49,7 +58,7 @@
      MenuSearchService.$inject = ['$http'];
      function MenuSearchService($http){
          var service = this;
-        var foundItems = [];
+        
          service.getMatchedMenuItems = function (searchTerm) { //input searchTerms
              return $http({
                  method: "GET",
@@ -57,15 +66,13 @@
              }).then(function(result){
                 
                  //alert(JSON.stringify(result));
-                
-                 
-                 for (var i = 0; i < result.data.menu_items.length; i++) {
-                    var description = result.data.menu_items[i].description;
-                    if (description.indexOf(searchTerm) !== -1) {
-                        foundItems.push(result.data.menu_items[i]);
+                var foundItems = [];
+                    for (var i = 0; i < result.data.menu_items.length; i++) {
+                        var description = result.data.menu_items[i].description;
+                        if (description.indexOf(searchTerm) !== -1) {
+                            foundItems.push(result.data.menu_items[i]);
+                        }
                     }
-                    }
-
  
                 return foundItems;
                   
